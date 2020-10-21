12138b870a7dc4b83bab4e3cddfc79a0de7ad457
LeonorPDX <leonoracolbert@gmail.com>
2020-10-21 15:00:22 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,44 @@
 export default (state = [], action) => {
-  return state;
+  let idx;
+  let quote;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+    
+    case 'REMOVE_QUOTE':
+      return state.filter(q => q.id !== action.quoteId);
+    
+    case 'UPVOTE_QUOTE':
+      idx = state.findIndex(q => q.id === action.quoteId);
+      quote = state[idx];
+      return [
+        ...state.slice(0, idx),
+      Object.assign({}, quote, {votes: quote.votes += 1}),
+      ...state.slice(idx + 1)
+      ]
+    
+    case 'DOWNVOTE_QUOTE':
+      idx = state.findIndex(q => q.id === action.quoteId);
+      quote = state[idx];
+      let updatedQ;
+      if (quote.votes > 0) {
+        updatedQ = {
+          ...quote,
+          votes: quote.votes -= 1
+        }
+       }
+      if (quote.votes === 0) {
+        updatedQ = {
+          ...quote
+        }
+      }
+      return [
+        ...state.slice(0, idx),
+      updatedQ,
+      ...state.slice(idx + 1)
+      ];
+      
+    default:
+      return state;
+  }
 }
